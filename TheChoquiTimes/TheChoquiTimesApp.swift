6c1428631ef3642d9715271e1caecefb227b5350
ives-algorhym <algoives@icloud.com>
2026-02-01 15:58:35 -0500

    Initial Commit

---
+++ TheChoquiTimes/TheChoquiTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheChoquiTimesApp.swift
+//  TheChoquiTimes
+//
+//  Created by Ives Murillo on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct TheChoquiTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheChoquiTimesTests/TheChoquiTimesTests.swift
@@ -0,0 +1,17 @@
+//
+//  TheChoquiTimesTests.swift
+//  TheChoquiTimesTests
+//
+//  Created by Ives Murillo on 2/1/26.
+//
+
+import Testing
+@testable import TheChoquiTimes
+
+struct TheChoquiTimesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
