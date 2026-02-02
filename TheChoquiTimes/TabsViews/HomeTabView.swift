f2148e5827f4373fd89a6e87bdbd17d65bec8de3
ives-algorhym <algoives@icloud.com>
2026-02-01 19:24:15 -0500

    Create HomeTabView

---
+++ TheChoquiTimes/TabsViews/HomeTabView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeTabView.swift
+//  TheChoquiTimes
+//
+//  Created by Ives Murillo on 2/1/26.
+//
+
+import SwiftUI
+
+struct HomeTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeTabView()
+}
