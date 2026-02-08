434085eafb06dfceac7dc4e1a78c9d72ac23c7af
ives-algorhym <algoives@icloud.com>
2026-02-07 21:01:02 -0500

    Link core to home feature

--- Packages/HomeFeature/Package.swift
+++ Packages/HomeFeature/Package.swift
@@ -14,11 +14,18 @@ let package = Package(
             name: "HomeFeature",
             targets: ["HomeFeature"]),
     ],
+    dependencies: [
+        .package(path: "../Core")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "HomeFeature"),
+            name: "HomeFeature",
+            dependencies: [
+                .product(name: "Core", package: "Core")
+            ]
+        ),
         .testTarget(
             name: "HomeFeatureTests",
             dependencies: ["HomeFeature"]
